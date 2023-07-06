6f92bbb33b78a41844eeb64caa32a717b155412f
richardhaddadau <richard@haddads.net.au>
2023-07-06 22:08:09 +1000

    build: set up splash

--- App.js
+++ App.js
@@ -1,20 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+
+import SplashScreen from "./src/Screens/SplashScreen";
 
 export default function App() {
+  // States
+  const [loading, setLoading] = useState(true);
+
+  setTimeout(() => {
+    // setLoading(false);
+  }, 1500);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      {loading ? (
+        <SplashScreen />
+      ) : (
+        <View style={styles.container}>
+          <Text>Open up App.js to start working on your app!</Text>
+        </View>
+      )}
+    </SafeAreaProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
