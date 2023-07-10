5e78558bddf92f1ff7ae1a965e96e1592d1e8bf2
richardhaddadau <richard@haddads.net.au>
2023-07-10 11:52:31 +1000

    fix: fontawesome import fixed on Empty Item

--- src/Components/HomeEmptyItem.js
+++ src/Components/HomeEmptyItem.js
@@ -1,5 +1,5 @@
 import { View, Text } from "react-native";
-import FontAwesome from "@expo/vector-icons";
+import { FontAwesome } from "@expo/vector-icons";
 
 const HomeEmptyItem = () => {
   return (
@@ -13,10 +13,27 @@ const HomeEmptyItem = () => {
       <Text style={{ color: "#b1b1b1" }}>
         No building is currently being tracked.
       </Text>
-      <Text style={{ color: "#b1b1b1" }}>
-        Tap the big <FontAwesome name="plus-circle" size={17} color="#d35322" />{" "}
-        to start.
-      </Text>
+      <View>
+        <Text style={{ color: "#b1b1b1" }}>
+          Tap the big{" "}
+          <View
+            style={{
+              transform: "translateY(4px)",
+              flexDirection: "row",
+              justifyContent: "center",
+              alignItems: "center",
+              width: 25,
+              height: 25,
+              borderWidth: 1,
+              borderColor: "#d35322",
+              borderRadius: 9999,
+            }}
+          >
+            <FontAwesome name="plus-circle" size={17} color="#d35322" />
+          </View>{" "}
+          to start.
+        </Text>
+      </View>
     </View>
   );
 };
