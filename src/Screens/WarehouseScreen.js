a07affb10a31df4a30ce3f316558a080f939f6af
richardhaddadau <richard@haddads.net.au>
2023-07-30 18:48:10 +1000

    dev: input next sorted

--- src/Screens/WarehouseScreen.js
+++ src/Screens/WarehouseScreen.js
@@ -6,19 +6,30 @@ import { observer } from "mobx-react";
 import StatInput from "../Components/StatInput";
 import { cleanUpNumber } from "../Helpers/CleanUps";
 import resourceStore from "../Helpers/ResourceStore";
+import { useNavigation } from "@react-navigation/native";
+import { useRef } from "react";
 
 const WarehouseScreen = observer(() => {
   // MobX
   const { stone, iron, zCoins, diamonds } = resourceStore;
 
+  // Refs
+  const stoneRef = useRef(null);
+  const ironRef = useRef(null);
+  const zCoinsRef = useRef(null);
+  const diamondsRef = useRef(null);
+
   const handleWarehouseChange = (resourceType, value) => {
     resourceStore.updateWarehouseQuantity(resourceType, parseInt(value, 10));
   };
+
+  const navigation = useNavigation();
 
   return (
     <KeyboardAwareScrollView>
       <View style={styles.container}>
         <StatInput
+          ref={stoneRef}
           label={"Stone in Warehouse"}
           value={stone.warehouse.toString()}
           onChangeValue={(text) => {
@@ -35,9 +46,12 @@ const WarehouseScreen = observer(() => {
               />
             )
           }
+          returnKeyType={"next"}
+          onSubmitEditing={() => ironRef.current.focus()}
         />
 
         <StatInput
+          ref={ironRef}
           label={"Iron in Warehouse"}
           value={iron.warehouse.toString()}
           onChangeValue={(text) => {
@@ -54,9 +68,12 @@ const WarehouseScreen = observer(() => {
               />
             )
           }
+          returnKeyType={"next"}
+          onSubmitEditing={() => zCoinsRef.current.focus()}
         />
 
         <StatInput
+          ref={zCoinsRef}
           label={"ZCoins in Warehouse"}
           value={zCoins.warehouse.toString()}
           onChangeValue={(text) => {
@@ -73,9 +90,12 @@ const WarehouseScreen = observer(() => {
               />
             )
           }
+          returnKeyType={"next"}
+          onSubmitEditing={() => diamondsRef.current.focus()}
         />
 
         <StatInput
+          ref={diamondsRef}
           label={"Diamonds in Warehouse"}
           value={diamonds.warehouse.toString()}
           onChangeValue={(text) => {
@@ -92,6 +112,8 @@ const WarehouseScreen = observer(() => {
               />
             )
           }
+          returnKeyType={"go"}
+          onSubmitEditing={() => navigation.navigate("Stone Chests")}
         />
 
         <View style={{ height: 50 }}></View>
